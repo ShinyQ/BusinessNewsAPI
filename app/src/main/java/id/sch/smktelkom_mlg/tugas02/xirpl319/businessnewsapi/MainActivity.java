32a73e4633a35219dc8cf1b991514583f0f66007
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2018-02-12 07:41:51 +0700

    + Adding getDataBerita() method + Adding Request Component For the API

--- app/src/main/java/id/sch/smktelkom_mlg/tugas02/xirpl319/businessnewsapi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas02/xirpl319/businessnewsapi/MainActivity.java
@@ -3,6 +3,15 @@ package id.sch.smktelkom_mlg.tugas02.xirpl319.businessnewsapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,5 +27,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getDataBerita(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, BASE_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e("Volley Error", toString());
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null){
+                    Log.e("Status Code", String.valueOf(networkResponse.statusCode));
+                }
+            }
+
+        }
+
+        );
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
