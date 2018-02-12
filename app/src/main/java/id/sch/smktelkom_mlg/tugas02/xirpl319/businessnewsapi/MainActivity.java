993662c5c416d4dd11fba9bcf284f3a5abcf0258
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2018-02-12 07:38:52 +0700

    Adding 4 Object In MAinActivity : + Adding String Object BASE_URL + Adding Object RecyclerView + Adding Object Adapter for RecyclerView + Adding List of ModelBerita called ModelBeritaList

--- app/src/main/java/id/sch/smktelkom_mlg/tugas02/xirpl319/businessnewsapi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas02/xirpl319/businessnewsapi/MainActivity.java
@@ -2,8 +2,17 @@ package id.sch.smktelkom_mlg.tugas02.xirpl319.businessnewsapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String BASE_URL = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4847be56cae34defb224fdfc1822d58f";
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    List<ModelBerita> ModelBeritaList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
